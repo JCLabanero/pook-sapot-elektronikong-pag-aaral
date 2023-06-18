a49453cf396b88534a97948bc0fe995c84c7626c
JCLabanero <johncarlolabanero@gmail.com>
2023-06-18 21:59:09 +0800

    update registration

--- js/register.js
+++ js/register.js
@@ -19,6 +19,33 @@ $(document).ready(function () {
             return;
         }
 
+        // if (!validatePassword(password)) {
+        //     alert("Invalid password. Please enter a valid password.");
+        //     return;
+        // }
+
+        switch(validatePassword(password)){
+            case 0:
+                break;
+            case 1:
+                alert("Passord too short");
+                return;
+            case 2:
+                alert("Include atleast one lowercase");
+                return;
+            case 3:
+                alert("Include atleast one uppercase");
+                return;
+            case 4:
+                alert("Include atleast one digit");
+                return;
+            case 5:
+                alert("Include atleast one special character");
+                return;
+            default:
+                alert("Error");
+        }
+
         // Create a data object to send to the server
         var data = {
             username: username,
@@ -37,11 +64,11 @@ $(document).ready(function () {
                     alert("Registration successful!");
                     // Redirect the user to the login page or perform any other necessary action
                 } else {
+                    if (response === "Email already exists") {
+                        $("#email").addClass("border-danger");
+                    }
                     if (response === "Username already exists") {
                         $("#username").addClass("border-danger");
-                    }
-                    if (response.toString() === "Email already exists") {
-                        
                     }
                     // alert(response); // Display the error message received from PHP
                 }
@@ -69,5 +96,37 @@ $(document).ready(function () {
         }
 
         return true;
+    }
+
+    // Validate password
+    function validatePassword(password) {
+        // Check password length
+        var minPasswordLength = 8; // Minimum length for password
+
+        if (password.length < minPasswordLength) {
+            return 1;
+        }
+
+        // Check for at least one lowercase letter
+        if (!/[a-z]/.test(password)) {
+            return 2;
+        }
+
+        // Check for at least one uppercase letter
+        if (!/[A-Z]/.test(password)) {
+            return 3;
+        }
+
+        // Check for at least one digit
+        if (!/[0-9]/.test(password)) {
+            return 4;
+        }
+
+        // Check for at least one special character
+        if (!/[!@#$%^&*]/.test(password)) {
+            return 5;
+        }
+
+        return 0;
     }
 });
