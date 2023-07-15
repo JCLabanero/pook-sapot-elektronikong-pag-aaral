18d5f4e87d60cdc2fd47dd685c1f54f6ff97d629
JCLabanero <johncarlolabanero@gmail.com>
2023-07-15 23:33:14 +0800

    update ajax requests with json file

--- login.php
+++ login.php
@@ -1,28 +1,32 @@
 <?php
-    require "php/session_check_out.php";
+require "php/session_check_out.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <main class="w-100 m-auto">
         <form id="loginForm" name="loginForm" method="post" class=" m-auto p-3 rounded" style="max-width: 350px;">
             <h1>Sign In</h1>
             <div class="mt-4 form-floating">
-                <input type="text" class="form-control rounded-0 rounded-top" id="user" placeholder="name@example.com" required>
+                <input type="text" class="form-control rounded-0 rounded-top" id="user" name="user" placeholder="name@example.com" required>
                 <label for="user" class="form-label">Username</label>
             </div>
             <div class="mb-3 form-floating">
-                <input type="password" class="form-control rounded-0 rounded-bottom" id="password" placeholder="Password123+_" required>
+                <input type="password" class="form-control rounded-0 rounded-bottom" id="password" name="password" placeholder="Password123+_" required>
                 <label for="password" class="form-label">Password</label>
+            </div>
+            <div class="mb-3 form-floating d-none" id="alert-message">
+                <p class="text-danger text-center">Alert message here!</p>
             </div>
             <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -37,9 +41,8 @@
         </form>
     </main>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" 
-crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script src="js/account.js"></script>
+
 </html>
--- php/account_authenticate.php
+++ php/account_authenticate.php
@@ -4,6 +4,11 @@ session_start();
 $usernameOrEmail = $_REQUEST["usernameOrEmail"];
 $password = $_REQUEST["password"];
 
+if ($usernameOrEmail == NULL || $password == NULL) {
+    createResponse(401, "The requested page needs a username and a password.");
+    return false;
+}
+
 // Your code to validate the login credentials
 // Example: Check if the username or email and password match an existing user in the XML
 $existingUser = false;
@@ -20,19 +25,29 @@ foreach ($users as $user) {
     if (strcasecmp($usernameOrEmail, $username) === 0 || strcasecmp($usernameOrEmail, $email) === 0) {
         $existingUser = true;
         if (password_verify($password, $storedPassword)) {
-            echo "success";
+            createResponse(200, "Login successfully.");
+            //set session
             $_SESSION["username"] = $username;
             $_SESSION["email"] = $email;
             $_SESSION["id"] = $id;
             exit;
         } else {
-            echo "Incorrect password.";
+            createResponse(102, "Password Incorrect.");
             exit;
         }
     }
 }
 
 if (!$existingUser) {
-    echo "User not found.";
+    createResponse(101, "User not found.");
     exit;
+}
+
+function createResponse($code, $message)
+{
+    $response = [
+        "status" => $code,
+        "message" => "$message"
+    ];
+    echo json_encode($response);
 }
