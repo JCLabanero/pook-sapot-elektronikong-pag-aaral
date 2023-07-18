e3629ffb134775cf1af9625d799f833ac9976d7f
JCLabanero <johncarlolabanero@gmail.com>
2023-07-18 22:41:58 +0800

    update adminmenu with new way of showing data

--- html/adminmenu.php
+++ html/adminmenu.php
@@ -13,14 +13,32 @@ require "../includes/start_in.php";
             <th scope="col">#</th>
             <th scope="col">Username</th>
             <th scope="col">Email</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
+            <th scope="col">Control</th>
           </tr>
         </thead>
         <tbody>
           <?php
           // Include the account manager PHP file
-          include('../php/account_manager.php');
+          include('../php/account_retrieve.php');
+          foreach ($xml->user as $user) {
+            $username = $user->username;
+            $email = $user->email;
+            $id = $user['id'];
+            if ($_SESSION["id"] == $id)
+              continue;
+          ?>
+            <tr>
+              <td><?php echo $id ?></td>
+              <td><?php echo $username ?></td>
+              <td><?php echo $email ?></td>
+              <td>
+                <button data-id="<?php echo $id ?>" data-username="<?php echo $username ?>" data-email="<?php echo $email ?>" class="btn btn-success"><i class='bi bi-pencil-square'></i></button>
+                <button data-id="<?php echo $id ?>" class="btn btn-danger"><i class='bi bi-trash'></i></button>
+                <?php ?>
+              </td>
+            </tr>
+          <?php
+          }
           ?>
         </tbody>
       </table>
---
+++ php/account_retrieve.php
@@ -0,0 +1,3 @@
+<?php
+// Read the account data from the XML file
+$xml = simplexml_load_file("../xml/accounts.xml");
